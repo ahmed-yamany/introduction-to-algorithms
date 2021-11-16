4392de74fc3290c4826a8b0534b6eb5bca738796
yamany <yamany2862016@gmail.com>
2021-11-16 02:50:47 +0200

    insertion sort nondecreasing & non decresing

--- instertion_sort.py
+++ instertion_sort.py
@@ -1,13 +1,27 @@
-def insertion_sort(array):
+def insertion_sort_non_decreasing(array):
     for i in range(1, len(array)):
         key = array[i]
-        j = i-1
+        j = i - 1
         while array[j] > key and j >= 0:
-            array[j+1] = array[j]
+            array[j + 1] = array[j]
             j = j - 1
         array[j + 1] = key
 
     return array
 
 
-insertion_sort([5, 2, 4, 6, 1, 3])
+insertion_sort_non_decreasing([31, 41, 59, 41, 58])
+
+
+def insertion_sort_non_increasing(array):
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+        while array[j] < key and j >= 0:
+            array[j + 1] = array[j]
+            j = j -1
+        array[j + 1] = key
+    return array
+
+
+insertion_sort_non_increasing([31, 41, 59, 41, 58])
